8757d7596c8be9c4705660c22708be68b0ea6700
engr-faysal <engr.mdfaysal@gmail.com>
2026-05-03 14:02:54 +0600

    Persistence
    Used Local Storage and for persistence. Json. set &get, save and render.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const addBtn = document.getElementById('add-btn');
 const inputTask = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function saveTasks() { 
+    localStorage.setItem('tasks',JSON.stringify(tasks));    
+}
+
+function renderTasks() {
+    todoList.innerHTML = '';
+
+    tasks.forEach((task) => {
+        const taskElement = createTaskElement(task);
+        todoList.appendChild(taskElement);
+    });
+}
 
 function createTaskElement(task) {
     const li = document.createElement('li');
@@ -8,14 +22,18 @@ function createTaskElement(task) {
     const taskText = document.createElement('span');
     const DltButton = document.createElement('button');
 
-    taskText.textContent = task;
-    DltButton.textContent = 'Delete';
-    DltButton.classList.add('btn', 'btn-sm' ,'btn-outline-danger','float-end');
-
-    completeBtn.textContent = "";
     completeBtn.classList.add('complete-btn');
     
+    taskText.textContent = task.title;
+
+    if(task.completed){
+        taskText.classList.add('completed');
+        //completeBtn.classList.add('done'); 
+         completeBtn.textContent = "✓";
+    }
     
+    DltButton.textContent = 'Delete';
+    DltButton.classList.add('btn', 'btn-sm' ,'btn-outline-danger','float-end');
 
     li.appendChild(completeBtn);
     li.appendChild(taskText);
@@ -24,36 +42,45 @@ function createTaskElement(task) {
     li.classList.add('d-flex', 'align-items-center', 'gap-2','justify-content-between', 'list-group-item', 'mb-2','w-100');
 
     completeBtn.addEventListener('click', () => {
-        taskText.classList.toggle('completed');
-        completeBtn.classList.toggle('done');
+        const targetTask = tasks.find((item)=>item.id ===task.id);
+
+        targetTask.completed = !targetTask.completed;
+        saveTasks();
+        renderTasks(); 
     });
 
     DltButton.addEventListener('click', (event) => {
     event.stopPropagation();
-    li.remove();
+    tasks = tasks.filter((item) => item.id !== task.id);
+    saveTasks();
+    renderTasks();
     })
 
     return li;
 }
-
+// Add Task
 addBtn.addEventListener('click', () => {
     const task = inputTask.value.trim();
     if(task === ''){
         alert('Please enter a task');
         return;
     }
-    
-
-    const taskElement = createTaskElement(task);
-    todoList.appendChild(taskElement);
+    const newTask={
+        id: Date.now(),
+        title: task,
+        completed: false
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks();
 
     inputTask.value = '';
-
-
-    
 }) 
+
 inputTask.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         addBtn.click();
     }
 });
+
+renderTasks();
